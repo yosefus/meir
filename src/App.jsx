82009ae8332da174a7c0a749dfc311fa18c3aa84
yosefus <yosalsoni@gmail.com>
2024-11-05 16:57:31 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from '../pages/Home';
+import Image from '../pages/Image';
+
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Home/>,
+  },
+  {
+    path: "img/:imageId",
+    element: <Image/>,
+  },
+])
+export default function App() {
+  return (
+    <div>
+        <RouterProvider router={router} />
+    </div>
+  )
+}
