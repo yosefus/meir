41827d76b3767baeaebc222dd5438ff5571ee3c9
yosefus <yosalsoni@gmail.com>
2024-11-05 17:15:42 +0200

    first

--- src/pages/Image.jsx
+++ src/pages/Image.jsx
@@ -5,8 +5,14 @@ export default function Image() {
   const { imageId } = useParams()
   const current =  images.find(image => image.id == imageId)
   return (
-    <main className='container mx-auto'>
-      Image - {imageId}
+    <main className='container mx-auto grid grid-cols-1 md:grid-cols-4'>
+      <div className='md:col-span-3 p-4' >
+
+      </div>
+      <div className='bg-red-50 p-4'>
+
+      </div>
+    
     </main>
   )
 }
